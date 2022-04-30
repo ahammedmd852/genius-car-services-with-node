af59b92e50d4e1f8a5711663b2d173e35e490856
ahammed <ahammedmd852@gmail.com>
2022-05-01 03:08:42 +0600

    Order page created

--- index.js
+++ index.js
@@ -44,11 +44,19 @@ async function run() {
             res.send(result);
         });
         // Order collection API
+        app.get('/order', async (req, res) => {
+            const email = req.query?.email;
+            const query = { email: email };
+            const cursor = orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+        });
+
         app.post('/order', async (req, res) => {
             const order = req.body;
             const result = await orderCollection.insertOne(order);
             res.send(result);
-        })
+        });
     }
     finally {
         // await client.close();
